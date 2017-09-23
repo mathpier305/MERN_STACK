9d858ef5bce4ffc25f37cd613990b5f713e177e0
math305 <mathpierdev305@gmail.com>
2017-09-23 15:54:04 -0400

    Event Handling Chapter4.4

--- static/App.js
+++ static/App.js
@@ -208,7 +208,8 @@ var IssueList = function (_React$Component5) {
     var _this5 = _possibleConstructorReturn(this, (IssueList.__proto__ || Object.getPrototypeOf(IssueList)).call(this));
 
     _this5.state = { issues: [] };
-    setTimeout(_this5.createTestIssue.bind(_this5), 2000);
+    _this5.createTestIssue = _this5.createTestIssue.bind(_this5);
+    setTimeout(_this5.createTestIssue, 2000);
     return _this5;
   }
 
@@ -255,6 +256,11 @@ var IssueList = function (_React$Component5) {
         React.createElement(IssueFilter, null),
         React.createElement('hr', null),
         React.createElement(IssueTable, { issues: this.state.issues }),
+        React.createElement(
+          'button',
+          { onClick: this.createTestIssue },
+          ' Add '
+        ),
         React.createElement('hr', null),
         React.createElement(IssueAdd, null)
       );
