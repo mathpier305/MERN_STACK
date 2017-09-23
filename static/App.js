3328c594959011466fcc3a72e30df2ad0e8df861
math305 <mathpierdev305@gmail.com>
2017-09-23 15:46:25 -0400

    Async State Initialization chapter4.3

--- static/App.js
+++ static/App.js
@@ -207,12 +207,26 @@ var IssueList = function (_React$Component5) {
 
     var _this5 = _possibleConstructorReturn(this, (IssueList.__proto__ || Object.getPrototypeOf(IssueList)).call(this));
 
-    _this5.state = { issues: issues };
+    _this5.state = { issues: [] };
     setTimeout(_this5.createTestIssue.bind(_this5), 2000);
     return _this5;
   }
 
   _createClass(IssueList, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      this.loadData();
+    }
+  }, {
+    key: 'loadData',
+    value: function loadData() {
+      var _this6 = this;
+
+      setTimeout(function () {
+        _this6.setState({ issues: issues });
+      }, 500);
+    }
+  }, {
     key: 'createIssue',
     value: function createIssue(newIssue) {
       var newIssues = this.state.issues.slice();
--- static/src/App.jsx
+++ static/src/App.jsx
@@ -74,8 +74,18 @@ title: 'Missing bottom border on panel',
 class IssueList extends React.Component{
     constructor(){
       super();
-      this.state = {issues:issues}
+      this.state = {issues: []};
       setTimeout(this.createTestIssue.bind(this), 2000);
+    }
+
+    componentDidMount(){
+      this.loadData();
+    }
+
+    loadData(){
+      setTimeout(()=>{
+        this.setState({issues: issues});
+      }, 500);
     }
 
     createIssue(newIssue){
