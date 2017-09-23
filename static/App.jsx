54e45988cce064110b644bbbcb8dc6da1d0826ba
math305 <mathpierdev305@gmail.com>
2017-09-23 14:27:05 -0400

    initial commit

---
+++ static/App.jsx
@@ -0,0 +1,3 @@
+var contentNode = document.getElementById('contents');
+var component = <h1 className="aColor">Hello World!</h1>;        // A simple component, written in JSX
+ReactDOM.render(component, contentNode);      // Render the component inside the content Node
