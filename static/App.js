df5aa5071a4a3a9183079e98d49a2ca5417fddc8
math305 <mathpierdev305@gmail.com>
2017-09-24 08:20:46 -0400

    The List API chapter5.1

--- static/App.js
+++ static/App.js
@@ -216,17 +216,6 @@ var IssueAdd = function (_React$Component4) {
   return IssueAdd;
 }(React.Component);
 
-var issues = [{
-  id: 1, status: 'Open', owner: 'Ravan',
-  created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
-  title: 'Error in console when clicking add'
-
-}, {
-  id: 2, status: 'Assigned', owner: 'Eddie',
-  created: new Date('2016-08-16'), effort: 14, completionDate: new Date('2016-08-30'),
-  title: 'Missing bottom border on panel'
-}];
-
 var IssueList = function (_React$Component5) {
   _inherits(IssueList, _React$Component5);
 
--- static/src/App.jsx
+++ static/src/App.jsx
@@ -83,19 +83,6 @@ class IssueAdd extends React.Component{
   }
 }
 
-const issues = [
-{
-  id: 1, status: 'Open', owner: 'Ravan',
-  created: new Date('2016-08-15'), effort: 5, completionDate : undefined,
-  title: 'Error in console when clicking add',
-
-},
-{
-id: 2, status: 'Assigned', owner: 'Eddie',
-created: new Date('2016-08-16'), effort: 14, completionDate : new Date('2016-08-30'),
-title: 'Missing bottom border on panel',
-},
-];
 class IssueList extends React.Component{
     constructor(){
       super();
