0d6910b7d3afca554c52a6404e58ded3bcf7a87a
math305 <mathpierdev305@gmail.com>
2017-10-14 01:41:57 -0400

    chapter 8 nested Routes working well but has error when click on filter menu

--- static/src/App.jsx
+++ static/src/App.jsx
@@ -1,5 +1,6 @@
 import 'babel-polyfill';
 import React from 'react';
+import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom';
 import {HashRouter as Router, Route, Switch, Redirect, hashHistory, withRouter} from 'react-router-dom';
 
@@ -9,17 +10,43 @@ import IssueEdit from './IssueEdit.jsx';
 
 const contentNode = document.getElementById('contents');
 const NoMatch = () => <p> Page Not Found </p>;
+
+const App = (props) =>(
+  <div>
+    <div className="header">
+      <h1> Issue Tracker </h1>
+    </div>
+    <div className="contents">
+      {props.children}
+    </div>
+    <div className="footer">
+      Full Source code available at this
+      <a href="https://github.com/vasansr/prop-mern-stack">
+      Github Repository
+      </a>
+    </div>
+  </div>
+);
+
+App.propTypes = {
+  children: PropTypes.object.isRequired,
+};
+
+
+
 const RoutedApp = () => (
     <Router history={hashHistory} >
-    <div>
+    <App>
+
     <Switch>
       <Route path="/issues/:id" component={IssueEdit}/>
       <Route path="/issues" component={withRouter(IssueList)} />
       <Redirect exact from="/" to="/issues"/>
       <Route path="*" component={NoMatch} />
-    </Switch>
 
-    </div>
+
+    </Switch>
+    </App>
   </Router>
 
 );
